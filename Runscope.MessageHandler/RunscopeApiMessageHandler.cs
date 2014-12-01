7aa2b63c87477e58bb4976ad4767034d5bcd952f
Shawn Kendrot <skendrot@sk-desktop.1rgs.com>
2014-12-01 13:17:43 -0700

    Create a way to filter requests/responses into different buckets

--- Runscope.MessageHandler/RunscopeApiMessageHandler.cs
+++ Runscope.MessageHandler/RunscopeApiMessageHandler.cs
@@ -13,14 +13,15 @@ namespace Runscope
 {
     public class RunscopeApiMessageHandler : DelegatingHandler
     {
+        private readonly Func<HttpRequestMessage, HttpResponseMessage, string> _bucketFilter;
         private readonly string _bucketKey;
         private readonly Func<HttpRequestMessage, HttpResponseMessage, bool> _filter;
         private readonly HttpClient _RunscopeClient;
 
-        public RunscopeApiMessageHandler(string authtoken, 
+        public RunscopeApiMessageHandler(string authtoken,
                                       string bucketKey,
                                       string runscopeApi = "https://api.runscope.com",
-                                      Func<HttpRequestMessage, HttpResponseMessage,bool> filter = null
+                                      Func<HttpRequestMessage, HttpResponseMessage, bool> filter = null
                                        )
         {
             _bucketKey = bucketKey;
@@ -30,20 +31,40 @@ namespace Runscope
                 BaseAddress = new Uri(runscopeApi)
             };
             _RunscopeClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", authtoken);
-            
+        }
+
+        public RunscopeApiMessageHandler(string authtoken,
+                                string runscopeApi = "https://api.runscope.com",
+                                Func<HttpRequestMessage, HttpResponseMessage, string> bucketFilter = null
+                                 )
+        {
+            _bucketFilter = bucketFilter;
+            _RunscopeClient = new HttpClient()
+            {
+                BaseAddress = new Uri(runscopeApi)
+            };
+            _RunscopeClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", authtoken);
         }
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
             CancellationToken cancellationToken)
         {
-            
+
             var runscopeRequest = new RunscopeRequest(request);  // Preread to ensure non-rewindable stream is not consumed by app
 
             var response = await base.SendAsync(request, cancellationToken);
 
+            string bucket = _bucketKey;
+            if (_bucketFilter != null)
+            {
+                bucket = _bucketFilter(request, response);
+            }
+
+            if (string.IsNullOrWhiteSpace(bucket)) return response;
+
             if (_filter == null || _filter(request, response))
             {
-                
+
                 var runscopeMessage = new RunscopeMessage()
                 {
                     Request = runscopeRequest,
